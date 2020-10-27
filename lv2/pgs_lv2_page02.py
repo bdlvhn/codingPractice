e6676f6b03f32c884f00e19f6da034aec04a08f1
bdlvhn <68417711+bdlvhn@users.noreply.github.com>
2020-10-27 23:36:32 +0900

    Update pgs_lv2_page02.py

--- lv2/pgs_lv2_page02.py
+++ lv2/pgs_lv2_page02.py
@@ -17,3 +17,28 @@ def solution(s):
             ans.append(word)
             isWord += 1
     return ''.join(ans)
+
+
+"""피보나치 수"""
+from functools import reduce
+
+def solution(n):
+    l = [0,1]
+    for i in range(n-1):
+        l.append(l[-1]+l[-2])
+    return l[-1]%1234567
+
+
+"""최솟값 만들기"""
+def solution(A,B):
+    A, B = A.sort(), B.sort()
+    ans = 0
+
+    for i in range(len(A)):
+        if A[0]*B[-1] >= A[-1]*B[0]:
+            ans += A.pop(0)*B.pop(-1)
+        else:
+            ans += A.pop(-1)*B.pop(0)
+    return ans
+
+
