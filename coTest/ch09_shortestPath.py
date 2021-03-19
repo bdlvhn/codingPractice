e932178593408556ce3cd8ac08371681268215fd
bdlvhn <bdlvhn@outlook.com>
2021-03-19 15:38:05 +0900

    Update ch09 chapter

--- coTest/ch09_shortestPath.py
+++ coTest/ch09_shortestPath.py
@@ -1,9 +1,9 @@
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
+import heapq
 
-
-### 9,예제 - 간단한 Dijkstra 알고리즘
+### 9, 간단한 Dijkstra 알고리즘
 n,m = map(int,input().split()) # 노드, 간선 갯수
 start = int(input())
 
@@ -48,15 +48,13 @@ for i in range(1,n+1):
   else:
     print(distance[i])
 
-
-### 9,예제 - 개선된 Dijkstra 알고리즘
+### 9, 개선된 Dijkstra 알고리즘
 import heapq
 
 n,m = map(int,input().split()) # 노드, 간선 갯수
 start = int(input())
 
 graph = [[] for i in range(n+1)]
-visited = [False] * (n+1)
 distance = [INF] * (n+1)
 
 for _ in range(m):
@@ -75,7 +73,6 @@ def dijkstra(start):
     
     for i in graph[now]:
       cost = dist + i[1]
-
       if cost < distance[i[0]]:
         distance[i[0]] = cost
         heapq.heappush(q,(cost,i[0]))
@@ -89,7 +86,7 @@ for i in range(1,n+1):
     print(distance[i])
 
 
-### 9,예제 - 플로이드 워셜 알고리즘
+### 9, 플로이드 워셜 알고리즘
 n,m = map(int,input().split()) # 노드, 간선 갯수
 graph = [[INF]*(n+1) for _ in range(n+1)]
 
@@ -113,75 +110,61 @@ for a in range(1,n+1):
       print(graph[a][b], end=" ")
   print()
 
-
-### 9,2 미래도시
-INF = int(1e9)
-
-def q09_2():
+def ch09_q02():
   n,m = map(int,input().split())
   graph = [[INF]*(n+1) for _ in range(n+1)]
-
-  for i in range(1,n+1):
+  for i in range(n):
     graph[i][i] = 0
-
+  
   for _ in range(m):
     a,b = map(int,input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
-
+    graph[a][b] = graph[b][a] = 1
+  
   x,k = map(int,input().split())
 
   for k in range(1,n+1):
     for a in range(1,n+1):
       for b in range(1,n+1):
         graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
+  answer = graph[1][k] + graph[k][x]
+  return answer if answer < INF else -1
 
-  for a in graph:
-    print(a)
+print(ch09_q02())
+
+def ch09_q03():
+  n,m,c = map(int,input().split())
+  graph = [[] for i in range(n+1)]
+  distance = [INF]*(n+1)
+
+  for _ in range(m):
+    x,y,z = map(int,input().split())
+    graph[x].append((y,z))
   
-  return graph[1][k] + graph[k][x] if (graph[1][k] + graph[k][x] < INF) else -1
+  def dijkstra(start):
+    q = []
+    heapq.heappush(q,(0,start))
+    distance[start]=0
 
-# print(q09_2())
+    while q:
+      dist,now = heapq.heappop(q)
+      
+      if distance[now] < dist:
+        continue
+      
+      for i in graph[now]:
+        cost = dist + i[1]
+        if cost < distance[i[0]]:
+          distance[i[0]] = cost
+          heapq.heappush(q,(cost,i[0]))
 
+  dijkstra(c)
+  
+  result = [0,0]
+  for i in range(1,n+1):
+    if i != c and distance[i] != INF:
+      result[0] += 1
+      result[1] = max(result[1],distance[i])
+  
+  return ' '.join(map(str,result))
 
-### 9,3 전보
-import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
-
-n,m,c = map(int,input().split())
-graph = [[] for i in range(n+1)]
-distance = [INF] * (n+1)
-
-for _ in range(m):
-  x,y,z = map(int,input().split())
-  graph[x].append((y,z))
-
-def dijkstra(start):
-  q = []
-  heapq.heappush(q,(0,start))
-  distance[start] = 0
-  while q:
-    dist, now = heapq.heappop(q)
-    if distance[now] < dist:
-      continue
-    
-    for i in graph[now]:
-      cost = dist + i[1]
-      if cost < distance[i[0]]:
-        distance[i[0]] = cost
-        heapq.heappush(q,(cost,i[0]))
-
-dijkstra(c)
-print(graph,distance)
-
-
-count = 0
-max_distance = 0
-for d in distance:
-  if d != INF:
-    count += 1
-    max_distance = max(max_distance,d)
-
-print(count-1,max_distance)
+print(ch09_q03())
