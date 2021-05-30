2b2855cd79e0cc35f4b6dab4812f16ba2d6ebd74
bdlvhn <bdlvhn@outlook.com>
2021-05-30 15:24:15 +0900

    Update ch11_greedy.py

--- coTest/2.problems/ch11_greedy.py
+++ coTest/2.problems/ch11_greedy.py
@@ -1,181 +1,101 @@
-# Ch11, 그리디 문제
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-# Q1 : 모험가 길드
-def ch11_q01():
-  n = int(input())
-  members = list(map(int,input().split()))
-  cnt = 1
+def q01():
+    n = int(input())
+    guild = [*map(int, input().split())]
+    guild.sort()
 
-  for i in range(2,n+1):
-    if members.cnt(i) >= i:
-      cnt += 1
-  return cnt
+    result = 0
+    count = 0
 
-# print(ch11_q01())
+    for person in guild:
+        count += 1
+        if count >= person:
+            result += 1
+            count = 0
 
-# Q2 : 곱하기 혹은 더하기
-def ch11_q02():
-  s = [int(i) for i in input()]
-  ans = s[0]
-  
-  for i in range(1,len(s)):
-    if (ans+s[i]) > (ans*s[i]):
-      ans += s[i]
-    else:
-      ans *= s[i]
-  
-  return ans
+    return result
 
-# print(ch11_q02())
+# print(q01())
 
-# Q3 : 문자열 뒤집기
-def ch11_q03():
-  s = [i for i in input()]
-  cnt = 0
-  print(s)
-  for j in range(len(s)-1):
-    if s[j] != s[j+1]:
-      cnt += 1
-  
-  return cnt-1
+def q02():
+    number = [int(n) for n in input().rstrip()]
+    result = number[0]
 
-# print(ch11_q03())
+    for i in range(1, len(number)):
+        result = max(result + number[i], result * number[i])
 
-# Q4 : 만들 수 없는 금액
-def ch11_q04():
-  n = int(input())
-  coins = list(map(int,input().split()))
-  coins.sort()
-  target = 1
+    return result
 
-  for coin in coins:
-    if coin==1:
-      target += 1
-    elif target >= coin:
-      target += coin
-    else:
-      return target
-  
-  return target
+# print(q02())
 
-# print(ch11_q04())
+def q03():
+    string = input().rstrip()
+    cnt = 0
+    for i in range(len(string) - 1):
+        if string[i] != string[i + 1]:
+            cnt += 1
+    return cnt // 2 + cnt % 2
 
-# Q05 : 볼링공 고르기
-from itertools import combinations
-def ch11_q05():
-  n,m = map(int,input().split())
-  weight = list(map(int,input().split()))
-  
-  return len([i for i in combinations(weight,2) if i[0] != i[1]])
+# print(q03())
 
-# print(ch11_q05())
+def q04():
+    n = int(input())
+    coins = [*map(int, input().split())]
+    coins.sort()
+    target = 1
 
-# Q06 : 무지의 먹방 라이브
+    for coin in coins:
+        if target < coin:
+            return target
+        else:
+            target += coin
 
+# print(q04())
 
+def q05():
+    n, m = map(int, input().split())
+    balls = [*map(int, input().split())]
+    array = [0] * (10 + 1)
 
+    for x in balls:
+        array[x] += 1
 
+    result = 0
+    for i in range(1, m + 1):
+        n -= array[i]
+        result += array[i] * n
 
-### answer code on the book
-# Q1 : 모험가 길드
-def ch11_q01b():
-  n = int(input())
-  data = list(map(int,input().split()))
-  data.sort()
-  group, count = 0
+    return result
 
-  for i in data:
-    count += 1
-    if count>=i:
-      group += 1
-      count = 0
-  return group
+# print(q05())
 
-# Q2 : 곱하기 혹은 더하기
-def ch11_q02b():
-  data = input()
-  result = int(data[0])
-
-  for i in range(1,len(data)):
-    num = int(data[i])
-    if num <= 1 or result <= 1:
-      result += num
-    else:
-      result *= num
-  
-  return result
-
-# Q3 : 문자열 뒤집기
-def ch11_q03b():
-  data = input()
-  count0 = 0
-  count1 = 0
-
-  if data[0]=='1':
-    count0 += 1
-  else:
-    count1 += 1
-  
-  for i in range(len(data)-1):
-    if data[i] != data[i+1]:
-      if data[i+1]=='1':
-        count0 += 1
-      else:
-        count1 += 1
-
-  return min(count0,count1)
-
-# Q4 : 만들 수 없는 금액
-def ch11_q04():
-  n = int(input())
-  data = list(map(int,input().split()))
-  data.sort()
-  target = 1
-
-  for x in data:
-    if target < x:
-      break
-    target += x
-  
-  return target
-
-  # Q05 : 볼링공 고르기
-def ch11_q05():
-  n,m = map(int,input().split())
-  data = list(map(int,input().split()))
-
-  array = [0] * 11
-  for x in data:
-    array[x] += 1
-  
-  result = 0
-  for i in range(1,m+1):
-    n -= array[i]
-    result += array[i] * n
-
-  return result
-
-# Q06 : 무지의 먹방 라이브
 import heapq
-def ch11_06(food_times,k):
-  if sum(food_times)<=k:
-    return -1
 
-  q = []
-  for i in range(len(food_times)):
-    heapq.heappush(q,(food_times[i],i+1))
-  
-  sum_value = 0
-  previous = 0
-  length = len(food_times)
+def q06(food_times, k):
+    if sum(food_times) <= k:
+        return -1
 
-  while sum_value + ((q[0][0]-previous)*length) <= k:
-    now = heapq.heappop(q)[0]
-    sum_value += (now - previous) * length
-    length -= 1
-    previous = now
-  
-  result = sorted(q, key=lambda x:x[1])
-  return result[(k-sum_value)%length][1]
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i + 1))
 
-print(ch11_06([3,1,2],5))
+    sum_value = 0
+    previous = 0
+    length = len(food_times)
+
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+
+    result = sorted(q, key=lambda x: x[1])
+    return result[(k - sum_value) % length][1]
+
+
+# food_times = [3, 1, 2]
+# k = 5
+# print(q06(food_times, k))
